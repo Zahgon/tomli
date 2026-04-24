ca43f50f6cce092d0cd16f91579ce393822a2e8a
danishs-lab <danish.samir@ethara.ai>
2026-04-24 15:12:44 +0530

    Commit 0

--- src/tomli/_re.py
+++ src/tomli/_re.py
@@ -62,34 +62,7 @@ def match_to_datetime(match: re.Match[str]) -> datetime | date:
     Raises ValueError if the match does not correspond to a valid date
     or datetime.
     """
-    (
-        year_str,
-        month_str,
-        day_str,
-        hour_str,
-        minute_str,
-        sec_str,
-        micros_str,
-        zulu_time,
-        offset_sign_str,
-        offset_hour_str,
-        offset_minute_str,
-    ) = match.groups()
-    year, month, day = int(year_str), int(month_str), int(day_str)
-    if hour_str is None:
-        return date(year, month, day)
-    hour, minute = int(hour_str), int(minute_str)
-    sec = int(sec_str) if sec_str else 0
-    micros = int(micros_str.ljust(6, "0")) if micros_str else 0
-    if offset_sign_str:
-        tz: tzinfo | None = cached_tz(
-            offset_hour_str, offset_minute_str, offset_sign_str
-        )
-    elif zulu_time:
-        tz = timezone.utc
-    else:  # local date-time
-        tz = None
-    return datetime(year, month, day, hour, minute, sec, micros, tzinfo=tz)
+    pass
 
 
 # No need to limit cache size. This is only ever called on input
@@ -97,23 +70,12 @@ def match_to_datetime(match: re.Match[str]) -> datetime | date:
 # 24 (hours) * 60 (minutes) * 2 (offset direction) = 2880.
 @lru_cache(maxsize=None)
 def cached_tz(hour_str: str, minute_str: str, sign_str: str) -> timezone:
-    sign = 1 if sign_str == "+" else -1
-    return timezone(
-        timedelta(
-            hours=sign * int(hour_str),
-            minutes=sign * int(minute_str),
-        )
-    )
+    pass
 
 
 def match_to_localtime(match: re.Match[str]) -> time:
-    hour_str, minute_str, sec_str, micros_str = match.groups()
-    sec = int(sec_str) if sec_str else 0
-    micros = int(micros_str.ljust(6, "0")) if micros_str else 0
-    return time(int(hour_str), int(minute_str), sec, micros)
+    pass
 
 
 def match_to_number(match: re.Match[str], parse_float: ParseFloat) -> Any:
-    if match.group("floatpart"):
-        return parse_float(match.group())
-    return int(match.group(), 0)
+    pass
